8552d0a49d15e53a9d96aee1a9853c9584050de9
VLuca997 <kulertoast997@gmail.com>
2023-07-23 16:39:43 +0200

    ADD flags languages MILESTONE 2.1

--- src/components/MovieComponent.vue
+++ src/components/MovieComponent.vue
@@ -9,6 +9,26 @@ export default {
             store
         } 
     },
+    methods:{
+        getCountryflag(flag){
+            if (flag == 'it' ){
+                return 'https://flagicons.lipis.dev/flags/4x3/it.svg'
+            }
+            else if (flag == 'ja'){
+                return 'https://flagicons.lipis.dev/flags/4x3/jp.svg'
+            }
+            else if (flag == 'us'){
+                return 'https://flagicons.lipis.dev/flags/4x3/us.svg'
+            }
+            else if (flag == 'de'){
+                return 'https://flagicons.lipis.dev/flags/4x3/de.svg'
+            }
+            else{
+                
+                return 'https://flagicons.lipis.dev/flags/4x3/un.svg'
+            }
+        }
+    }
     
 }
 </script>
@@ -20,7 +40,7 @@ export default {
                 <ol>
                     <li>{{ movie.title }}</li>
                     <li>{{ movie.original_title }}</li>
-                    <li>{{ movie.original_language }}</li>
+                    <li><img :src="getCountryflag(movie.original_language)" alt=""></li>  <!--V-BIND SU IMG-->
                     <li>{{ movie.vote_average }}</li>
 
                 </ol>
@@ -29,7 +49,10 @@ export default {
     </div>
 </template>
 <style lang="scss" scoped>
-
+    img{
+        max-width: 30px;
+        border: 1px solid black;
+    }
 
 
 </style>
